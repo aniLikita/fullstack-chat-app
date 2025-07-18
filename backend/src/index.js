e32741b5c7bfc7f422e9d72ba985d10dc10fb7c8
unknown <aningalikita@gmail.com>
2025-07-19 00:09:42 +0100

    Deployment Please

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,16 +12,14 @@ import { app , server } from './lib/socket.js';
 
 dotenv.config();
 
-const BASE_URL= import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
-
 app.use(cors({
-  origin: BASE_URL,
+  origin:'http://localhost:5173',
   credentials: true,
 }))
 
