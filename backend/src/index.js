887f7a6e3ba1851015745a3382e578a7b94d726e
unknown <aningalikita@gmail.com>
2025-07-19 11:18:31 +0100

    Deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,9 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
+const rootPath = process.cwd();
+const distPath = path.join(rootPath, "frontend", "dist");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,10 +31,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(distPath));
+  
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
 
